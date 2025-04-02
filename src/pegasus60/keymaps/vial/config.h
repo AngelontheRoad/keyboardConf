72581d9a73822b1a67972c6261be630f062f867b
AngelontheRoad <gjr2012477@gmail.com>
2025-04-02 19:50:08 +0800

    update haven60 lily pegasus60 config

---
+++ src/pegasus60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x0B, 0x21, 0x9F, 0xAC, 0xD9, 0xCF, 0x32, 0xDC}
+#define VIAL_UNLOCK_COMBO_ROWS                                                 \
+    { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS                                                 \
+    { 0, 13 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
