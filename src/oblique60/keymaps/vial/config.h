3715cdc96c991ab7e1993afdd57b0ee586be252e
AngelontheRoad <gjr2012477@gmail.com>
2026-01-22 20:10:01 +0800

    fix: oblique spacebar key must be assigned explicitly

---
+++ src/oblique60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x96, 0xA2, 0x9F, 0x23, 0x70, 0x4C, 0xF2, 0x7C}
+#define VIAL_UNLOCK_COMBO_ROWS                                                 \
+    { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS                                                 \
+    { 0, 13 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
